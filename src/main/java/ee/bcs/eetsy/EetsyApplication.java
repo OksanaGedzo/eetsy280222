df4dd6cd46144b998d70a33f01787f910369ebbf
Rait Rotsan <rait.r@hotmail.com>
2022-02-11 16:15:02 +0200

    Initial commit

---
+++ src/main/java/ee/bcs/eetsy/EetsyApplication.java
@@ -0,0 +1,13 @@
+package ee.bcs.eetsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EetsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EetsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/bcs/eetsy/EetsyApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bcs.eetsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EetsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
