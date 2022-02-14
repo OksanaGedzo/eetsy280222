822a344f2e9939e4486eaf7ee11af4d270877a31
Rait Rotsan <rait.r@hotmail.com>
2022-02-14 15:03:19 +0200

    Added create_data.sql

---
+++ db/create_data.sql
@@ -0,0 +1,6 @@
+-- User role data
+INSERT INTO public.role (id, description) VALUES (DEFAULT, 'Administrator');
+INSERT INTO public.role (id, description) VALUES (DEFAULT, 'Moderator');
+INSERT INTO public.role (id, description) VALUES (DEFAULT, 'Seller');
+INSERT INTO public.role (id, description) VALUES (DEFAULT, 'User');
+
