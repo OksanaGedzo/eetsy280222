6ffddd6ab983797767205a30d295162863eee918
merikese <merekivi@gmail.com>
2022-02-18 16:16:01 +0200

    added order dto

---
+++ src/main/java/ee/bcs/eetsy/domain/shoppingcart/OrderDto.java
@@ -0,0 +1,20 @@
+package ee.bcs.eetsy.domain.shoppingcart;
+
+import ee.bcs.eetsy.domain.paymentmethod.PaymentMethodDto;
+import ee.bcs.eetsy.domain.user.UserDto;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+public class OrderDto implements Serializable {
+    private final Integer id;
+    private final UserDto user;
+    private final PaymentMethodDto paymentMethod;
+    private final String orderNumber;
+    private final Instant orderDate;
+    private final String orderStatus;
+    private final BigDecimal totalPrice;
+}
